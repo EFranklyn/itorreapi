36aab6184b4c329cbd8754f84072664d56a03bc3
Elisson Franklyn <elisson.franklyn@gmail.com>
2021-03-02 00:36:22 -0300

    cuidando do visual e tela de login

--- itorre/settings.py
+++ itorre/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  env('DEBUG')
+DEBUG =  True
 
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS')] #env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [] #env('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -127,8 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR,env('STATIC'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+#STATIC_ROOT =  os.path.join(BASE_DIR,env('STATIC'))
 
 
 REST_FRAMEWORK = {
